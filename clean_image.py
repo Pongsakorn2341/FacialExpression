aed7a74eb4f463fe78e7d180848450408d40cf82
Tottee <meepanda2341@gmail.com>
2022-02-09 21:38:27 +0700

    initial commit

---
+++ clean_image.py
@@ -0,0 +1,48 @@
+from imutils import paths
+import argparse
+import cv2
+
+def variance_of_laplacian(image):
+	# compute the Laplacian of the image and then return the focus
+	# measure, which is simply the variance of the Laplacian
+	return cv2.Laplacian(image, cv2.CV_64F).var()
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True,
+	help="./images")
+ap.add_argument("-t", "--threshold", type=float, default=10.0,
+	help="focus measures that fall below this value will be considered 'blurry'")
+args = vars(ap.parse_args())
+count = 0
+for imagePath in paths.list_images(args["images"]):
+    image = cv2.imread(imagePath)
+    dimensions = image.shape
+    if(dimensions[0] != 350 or dimensions[1] != 350):
+        continue
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    fm = variance_of_laplacian(gray)
+    text = "Not Blurry"
+    if fm < args["threshold"]:
+        text = "Blurry"
+    else:
+        cv2.imshow("graycsale image", image)
+        break
+        count += 1
+print("Total count : ", count)
+# for imagePath in paths.list_images(args["images"]):
+# 	# load the image, convert it to grayscale, and compute the
+# 	# focus measure of the image using the Variance of Laplacian
+# 	# method
+# 	image = cv2.imread(imagePath)
+# 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# 	fm = variance_of_laplacian(gray)
+# 	text = "Not Blurry"
+# 	# if the focus measure is less than the supplied threshold,
+# 	# then the image should be considered "blurry"
+# 	if fm < args["threshold"]:
+# 		text = "Blurry"
+# 	# show the image
+# 	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+# 		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+# 	cv2.imshow("Image", image)
+# 	key = cv2.waitKey(0)
