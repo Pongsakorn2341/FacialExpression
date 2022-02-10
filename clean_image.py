90eadcadab5350f57d7a8307f636ee4d655327b7
Tottee <meepanda2341@gmail.com>
2022-02-10 20:54:56 +0700

    update code

--- clean_image.py
+++ clean_image.py
@@ -20,38 +20,34 @@ for imagePath in paths.list_images(args["images"]):
     dimensions = image.shape
     if(dimensions[0] != 350 or dimensions[1] != 350):
         continue
+    # if ..... write image opencv python
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     fm = variance_of_laplacian(gray)
-    text = "Not Blurry"
     if fm < args["threshold"]:
-        text = "Blurry"
-    else:
-        count += 1
-        print(imagePath);
-        allImage.append(imagePath);
-        # cv2.imshow('graycsale image',image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        continue
+    count += 1
+    print(imagePath);
+    allImage.append(imagePath);
+    # cv2.imshow('graycsale image',image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 print("Total count : ", count)
 f = open("demofile2.txt", "a")
 print("\n".join(allImage))
 f.write("\n".join(allImage))
 f.close()
-# for imagePath in paths.list_images(args["images"]):
-# 	# load the image, convert it to grayscale, and compute the
-# 	# focus measure of the image using the Variance of Laplacian
-# 	# method
-# 	image = cv2.imread(imagePath)
-# 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# 	fm = variance_of_laplacian(gray)
-# 	text = "Not Blurry"
-# 	# if the focus measure is less than the supplied threshold,
-# 	# then the image should be considered "blurry"
-# 	if fm < args["threshold"]:
-# 		text = "Blurry"
-# 	# show the image
-# 	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-# 		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-# 	cv2.imshow("Image", image)
-# 	key = cv2.waitKey(0)
+
+
+# 1. filter color of image (new)
+# rgb to gray scale
+
+# 2. face detect (aom)
+
+
+
+
+
+
+
+
