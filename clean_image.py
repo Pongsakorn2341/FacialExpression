f1f763d47bab9dd9cecd13d4d54ecd5123663405
Tottee <meepanda2341@gmail.com>
2022-02-12 12:42:11 +0700

    update refactor code & and time range

--- clean_image.py
+++ clean_image.py
@@ -1,6 +1,7 @@
 from imutils import paths
 import argparse
 import cv2
+import time
 
 def variance_of_laplacian(image):
 	# compute the Laplacian of the image and then return the focus
@@ -13,37 +14,35 @@ ap.add_argument("-i", "--images", required=True,
 ap.add_argument("-t", "--threshold", type=float, default=10.00,
 	help="focus measures that fall below this value will be considered 'blurry'")
 args = vars(ap.parse_args())
-
+start = time.time()
+print("Start Time : ", start)
 count = 0
 allImage = []
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 for imagePath in paths.list_images(args["images"]):
     image = cv2.imread(imagePath)
-
     dimensions = image.shape
     if(dimensions[0] != 350 or dimensions[1] != 350):
         continue
-
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     fm = variance_of_laplacian(gray)
     if fm < args["threshold"]:
         continue
-    
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     face = face_cascade.detectMultiScale(gray, 1.1, 4)
     if len(face) == 0:
-        print(imagePath)
+        # print(imagePath)
         continue
     
     count += 1
-    # print(imagePath);
+    print(imagePath);
     allImage.append(imagePath)
     # cv2.imshow('graycsale image',image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-
 print("Total count : ", count)
+end = time.time()
+print("Time range : ", end - start)
 f = open("demofile2.txt", "a")
-print("\n".join(allImage))
 f.write("\n".join(allImage))
 f.close()
 
