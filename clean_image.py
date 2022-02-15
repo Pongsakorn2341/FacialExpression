03364f0e8f02c07cffdc63b66c7a997ea515b76d
Natnicha Faksang <omsin.natnicha@gmail.com>
2022-02-15 11:23:10 +0700

    Create floder and collect cleaned images

--- clean_image.py
+++ clean_image.py
@@ -1,8 +1,10 @@
+from cv2 import resize
 from imutils import paths
 import argparse
 import cv2
 import time
 import os
+import shutil
 
 def variance_of_laplacian(image):
 	# compute the Laplacian of the image and then return the focus
@@ -15,39 +17,58 @@ ap.add_argument("-i", "--images", required=True,
 ap.add_argument("-t", "--threshold", type=float, default=10.00,
 	help="focus measures that fall below this value will be considered 'blurry'")
 args = vars(ap.parse_args())
+
 start = time.time()
 print("Start Time : ", start)
+
 count = 0
 allImage = []
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-path = 'D:\Project\\facial_cleandata\\new_image'
+# create cleaned_images floder
+path = os.getcwd()
+dir = path + '\\cleaned_images'
+if os.path.exists(dir):
+    shutil.rmtree(dir)
+os.makedirs(dir)
+
 for imagePath in paths.list_images(args["images"]):
     image = cv2.imread(imagePath)
-    dimensions = image.shape
-    if(dimensions[0] != 350 or dimensions[1] != 350):
-        continue
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # resize image
+    dim = (350, 350)
+    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+    # change color
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
     fm = variance_of_laplacian(gray)
+
+    # check blurry
     if fm < args["threshold"]:
+        # print(imagePath)
         continue
+
+    # detect face
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     face = face_cascade.detectMultiScale(gray, 1.1, 4)
     if len(face) == 0:
         # print(imagePath)
         continue
     
     count += 1
-    #print(imagePath)
-    cv2.imwrite(os.path.join(path , 'grayScale'+str(count)+'.jpg'), gray)
-    print(os.path.join(path , 'grayScale'+str(count)+'.jpg'))
+
+    cv2.imwrite(os.path.join(dir , (imagePath.split('\\'))[1]), gray)
+
     allImage.append(imagePath)
     # cv2.imshow('graycsale image',image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
+
 print("Total count : ", count)
+
 end = time.time()
 print("Time range : ", end - start)
-f = open("demofile2.txt", "a")
+
+f = open("cleanedImage.txt", "a")
 f.write("\n".join(allImage))
 f.close()
 
